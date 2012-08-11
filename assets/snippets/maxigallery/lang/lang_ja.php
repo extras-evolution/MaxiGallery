67f6bf8832150b2d13740da911238f10de358541
yama <yamamoto@kyms.ne.jp>
2012-08-11 09:23:40 +0900

    Add Japanese langeage

---
+++ assets/snippets/maxigallery/lang/lang_ja.php
@@ -0,0 +1,51 @@
+<?php
+// English language file
+// If you are going to make your own language file and are usign the lighbox, 
+// make a language file for it too, f.ex. /maxigallery/slidebox/slidebox_lang_en.js
+$strings['previous']="&laquo; 前へ";
+$strings['next']="次へ &raquo;";
+$strings['click_to_zoom']=" &raquo; クリックで拡大 -&gt;";
+$strings['click_to_go_back']="&lt;- クリックで戻る";
+$strings['click_to_open_original']= "クリックで拡大";
+$strings['back_to_normal_view'] = "画像管理を終了する";
+$strings['index']="index";
+$strings['go_to_gallery']="この画像ギャラリーを開く -&gt;";
+$strings['pictures_successfully_uploaded']="<b>画像をアップロードしました</b>";
+$strings['changes_have_been_saved']="<b>変更が保存されました</b>";
+$strings['delete']="削除";
+$strings['hide']="隠す";
+$strings['title']="タイトル";
+$strings['save_changes']="変更を保存";
+$strings['upload_pictures']="画像をアップロード";
+$strings['check_to_delete_this_picture']="この画像を消去";
+$strings['check_to_hide_this_picture']="この画像をギャラリーから隠す";
+$strings['manage_pictures']="画像の管理";
+$strings['date']="日付";
+$strings['description']="説明";
+$strings['position']="画像の並び順";
+$strings['gallery_description']="ギャラリーの説明";
+$strings['db_no_descr_support']="<em>注意: Gallery database does not support descriptions!</em><br />";
+$strings['db_no_pos_support']="<em>注意: Gallery database does not support picture positions!</em><br />";
+$strings['picture'] = "Picture:";
+$strings['max_pics_reached'] = "<br />You have reached maximum pictures limit for this gallery, so you can't send more pictures.";
+$strings['supported_types'] = "<b>Incorrect filetype!</b> This gallery supports the following filetypes: jpg/jpeg, png, gif and zip (which has aforementioned filetypes inside)";
+$strings['gif_not_supported'] = "<b>The GD-library on this server does not support .gif images</b> You must use only jpg and png images.";
+$strings['supported_types_inzip'] = "エラー: zip archive contained incorrect filetypes which were ignored.";
+$strings['max_pics_reached_some_discarded'] = "<b>Maximum pictures limit reached!</b><br />Some of the pictures sent might got discarded.";
+$strings['zip_not_supported'] = "<b>Your PHP-installation does not support .zip archives!</b> You have to install the <a href=\"http://www.php.net/manual/ref.zip.php\">PHP zip module and ZZIPlib library.</a>";
+$strings['database_error'] = "エラー: Gallery database could not be created!";
+$strings['database_error_field'] = "エラー: Could not create field to database table: ";
+$strings['invalid_class'] = "エラー: Cannot find maxigallery class file. Please check it! ";
+$strings['invalid_name_class'] = "エラー: ファイル名が無効です";
+$strings['delete_all_pictures'] = "画像を全て削除";
+$strings['delete_all_confirmation'] = "本当に画像を削除しますか？";
+$strings['pictures'] = "画像";
+$strings['jump_to_upload_form'] = "画像アップロードへ";
+$strings['back_to_top'] = "トップへ戻る";
+$strings['sort_pictures'] = "画像の並び替え";
+$strings['dragdesc'] = "画像の表示順をマウス操作で並び替えます";
+$strings['cancel'] = "キャンセル";
+$strings['number_allowed'] = "Number of pictures allowed to upload";
+$strings['gallery_resynched'] = "<b>ギャラリーを同期し直しました</b>";
+$strings['resynch'] = "ギャラリーを再読込み";
+?>
